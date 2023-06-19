8aae0bdbc402179e86130ae1b736a85560ec1247
silviatanas <silviatanas@abv.bg>
2023-06-19 12:01:59 +0300

    first draft of writing into csv

--- src/main/java/com/silviatanas/project/inventory/Contract.java
+++ src/main/java/com/silviatanas/project/inventory/Contract.java
@@ -1,7 +1,5 @@
 package com.silviatanas.project.inventory;
 
-import java.util.Date;
-
 public class Contract {
     String contractNumber;
     long startDate;
@@ -11,4 +9,68 @@ public class Contract {
     String billingSystemId;
     Environment[] environment;
     String[] relatedContracts;
+
+    public String getContractNumber() {
+        return contractNumber;
+    }
+
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
+    }
+
+    public long getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(long startDate) {
+        this.startDate = startDate;
+    }
+
+    public long getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(long endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getBillingSystemId() {
+        return billingSystemId;
+    }
+
+    public void setBillingSystemId(String billingSystemId) {
+        this.billingSystemId = billingSystemId;
+    }
+
+    public Environment[] getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(Environment[] environment) {
+        this.environment = environment;
+    }
+
+    public String[] getRelatedContracts() {
+        return relatedContracts;
+    }
+
+    public void setRelatedContracts(String[] relatedContracts) {
+        this.relatedContracts = relatedContracts;
+    }
 }
--- src/main/java/com/silviatanas/project/inventory/Main.java
+++ src/main/java/com/silviatanas/project/inventory/Main.java
@@ -7,10 +7,18 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 public class Main {
     public static void utility(File uncheckedFolder, File checkedFolder) {
         Gson gson = new Gson();
+
+        //List<Contract> contractList = new ArrayList<>();
+
+        // proper date formatting for the milliseconds data
+        DateFormat dateConvert = new SimpleDateFormat("yyyy-MM-dd");
 
         BufferedWriter writer = null;
         try {
@@ -18,25 +26,40 @@ public class Main {
 
             // validate if folder paths already exist
             for (File file : uncheckedFolder.listFiles()) {
+                System.out.println("3");
                 if (file.getAbsolutePath().endsWith(".json")) {
+                    System.out.println("4");
                     // convert json string to contract object
                     String fileContent = null;
                     try {
                         fileContent = Files.readString(file.toPath());
+                        System.out.println("5");
 
                         Contract contract = gson.fromJson(fileContent, Contract.class);
+                        //contractList.add(contract);
+
+                        // make checked folder if it doesn't exist
+                        if (!checkedFolder.exists()) {
+                            checkedFolder.mkdir();
+                            System.out.println("6");
+                        }
 
                         // move processed files
-                        if (checkedFolder.exists() && checkedFolder.isDirectory()) {
-                            Files.move(Paths.get(file.getAbsolutePath()), Paths.get(checkedFolder.getAbsolutePath()), StandardCopyOption.REPLACE_EXISTING);
-                        } else {
-                            checkedFolder.mkdir();
-                            Files.move(Paths.get(file.getAbsolutePath()), Paths.get(checkedFolder.getAbsolutePath()), StandardCopyOption.REPLACE_EXISTING);
+                        if (checkedFolder.isDirectory()) {
+                            System.out.println("7");
+                            Files.move(Paths.get(file.getAbsolutePath()), Paths.get(checkedFolder + File.separator + file.getName()), StandardCopyOption.REPLACE_EXISTING);
                         }
 
                         // data written into csv
-                        writer.write(contract.toString());
+                        writer.write(contract.getContractNumber() + ", ");
+                        writer.write(contract.getCustomerId() + ", ");
+                        writer.write(contract.getCustomerName() + ", ");
+                        writer.write(dateConvert.format(contract.getStartDate()) + ", ");
+                        writer.write(dateConvert.format(contract.getEndDate()) + ", ");
+                        writer.write(Arrays.toString(contract.getEnvironment()) + ", ");
+                        writer.write(Arrays.toString(contract.getRelatedContracts()));
                         writer.newLine();
+                        writer.flush();
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
@@ -49,7 +72,7 @@ public class Main {
 
     public static void main(String[] args) {
         // setting folder paths
-        String downloadPath = System.getProperty("user.home") + File.separator + "Download";
+        String downloadPath = System.getProperty("user.home") + File.separator + "Downloads";
         File uncheckedFolder = new File(downloadPath + File.separator + "Unchecked");
         File checkedFolder = new File(downloadPath + File.separator + "Checked");
         System.out.println("1");
